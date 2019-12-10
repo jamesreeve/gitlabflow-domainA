dd187a621a76858efb2998f5c874f5e5b8c3c94a
James Reeve <james.reeve@westpac.com.au>
2019-12-11 10:20:16 +1100

    JIRA-0000 Adding account object into domain

---
+++ GitLabFlow-DomainA/GitLabFlow_DomainA.h
@@ -0,0 +1,19 @@
+//
+//  GitLabFlow_DomainA.h
+//  GitLabFlow-DomainA
+//
+//  Created by James Reeve on 11/12/19.
+//  Copyright © 2019 James Reeve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitLabFlow_DomainA.
+FOUNDATION_EXPORT double GitLabFlow_DomainAVersionNumber;
+
+//! Project version string for GitLabFlow_DomainA.
+FOUNDATION_EXPORT const unsigned char GitLabFlow_DomainAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitLabFlow_DomainA/PublicHeader.h>
+
+
